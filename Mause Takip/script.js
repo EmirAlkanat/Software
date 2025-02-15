52b7434e9043cf9ea3dd70aead5e2996353a6e0e
Emir Alkanat <emiralknt@gmail.com>
2025-02-16 00:16:41 +0300

    yeni

---
+++ Mause Takip/script.js
@@ -0,0 +1,12 @@
+let container = document.querySelector('.container');
+for(let i = 0; i < 12; i++){
+  let row = document.createElement('div');
+  row.classList.add('row');
+  for (let j = 0; j < 20; j++){
+    let box = document.createElement('div');
+    box.classList.add('box');
+    box.style.filter = `hue-rotate(${i * 36}deg)`;
+    row.appendChild(box);
+  }
+  container.appendChild(row);
+}
